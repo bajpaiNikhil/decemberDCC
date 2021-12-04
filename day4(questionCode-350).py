2b1f13bf90ad72e16b5574dacbce7de4e5c378cd
NikhilBajpai <nikhil.cse16@gmail.com>
2021-12-04 19:29:42 +0530

    day 4

--- day4(questionCode-350).py
+++ day4(questionCode-350).py
@@ -3,4 +3,9 @@
 nums1 = [4,9,5]
 nums2 = [9,4,9,8,4]
 
-
+l = []
+for i in range(len(nums1)):
+    if nums1[i] in nums2:
+        l.append(nums1[i])
+        del nums2[nums2.index(nums1[i])]
+print(l)
