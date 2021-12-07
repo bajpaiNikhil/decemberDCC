105b1cb25f7a7e5ca10211cfeae04dcc9bbfc0ab
NikhilBajpai <nikhil.cse16@gmail.com>
2021-12-07 10:21:48 +0530

    day 7

---
+++ day7(questionCode-1290).py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+def toList(root):
+    l = ""
+    while(root):
+        l+=str(root.val)
+        root = root.next
+    return int(l,2)
+
+
+
+root = ListNode(1)
+root.next = ListNode(0)
+root.next.next = ListNode(0)
+root.next.next.next = ListNode(1)
+print(toList(root))
