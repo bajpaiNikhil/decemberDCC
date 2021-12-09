244edba935da95d3c96c1de9d4f690fd2bbbf323
NikhilBajpai <nikhil.cse16@gmail.com>
2021-12-09 19:09:08 +0530

    day 9

--- day8(questionCode-141).py
+++ day8(questionCode-141).py
@@ -1,5 +1,5 @@
 
-
 class Node:
     def __init__(self, val , next = None):
+        self.next = next
         self.val = val
---
+++ day9(questionCode-1306).py
@@ -0,0 +1,10 @@
+def canReach(A, i):
+    if 0 <= i < len(A) and A[i] >= 0:
+        A[i] = -A[i]
+        return A[i] == 0 or canReach(A, i + A[i]) or canReach(A, i - A[i])
+    return False
+
+A = [4,2,3,0,3,1,2]
+start = 5
+
+print(canReach(A,start))
