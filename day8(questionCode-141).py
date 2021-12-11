fb576367748ea368fe9a44eee05e9df39920b464
NikhilBajpai <nikhil.cse16@gmail.com>
2021-12-11 20:31:05 +0530

    day 10

--- day8(questionCode-141).py
+++ day8(questionCode-141).py
@@ -3,3 +3,23 @@ class Node:
     def __init__(self, val , next = None):
         self.next = next
         self.val = val
+
+def findcycle(root):
+
+    show = root
+    fast = root.next
+    while show is not fast:
+        show = show.next
+        fast = fast.next.next
+        return True
+    return False
+
+
+
+root = Node(3)
+root.next = Node(2)
+root.next.next = Node(0)
+root.next.next.next = Node(4)
+
+
+print(findcycle(root))
